7bb0797b8184592da26da8089dc09491aafaa376
misael <21170010a@gmail.com>
2025-01-08 06:39:23 -0600

    Se hace plantilla base, se pone el banner

--- salvando_patitas/urls.py
+++ salvando_patitas/urls.py
@@ -21,3 +21,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('salvando_patas.urls'))
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
